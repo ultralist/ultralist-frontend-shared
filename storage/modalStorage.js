8cec7cb32a96a94ca9d12b193064cd3cdcee9bdc
Grant Ammons <gammons@gmail.com>
2020-05-22 14:51:22 -0400

    add more shit to modalStorage

--- storage/modalStorage.js
+++ storage/modalStorage.js
@@ -2,15 +2,29 @@
 import { Storeable } from "./storeable"
 
 export default class ModalStorage {
+  storage: Storeable
+
   constructor(storage: Storeable) {
     this.storage = storage
   }
 
-  isModalOpen(): boolean {
-    return this.storage.load("modalOpen") === true
+  isModalOpen(name: ?string): boolean {
+    if (!name) {
+      return this.storage.load("modalOpen") === true
+    }
+
+    return (
+      this.storage.load("modalOpen") === true &&
+      this.storage.load("modalOpenName") === name
+    )
   }
 
-  setModalIsOpen(open: boolean) {
+  getModalOpenName(): string {
+    return this.storage.load("getModalOpenName")
+  }
+
+  setModalIsOpen(open: boolean, name: string) {
     this.storage.save("modalOpen", open)
+    this.storage.save("modalOpenName", open ? name : null)
   }
 }
