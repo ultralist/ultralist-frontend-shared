73d3385469db7c791fb7698602024f3ed4d0bcee
Grant Ammons <gammons@gmail.com>
2020-06-04 12:08:15 -0400

    fix bug with active api keys

--- models/apiKey.js
+++ models/apiKey.js
@@ -4,6 +4,7 @@ type ConstructorArgs = {
   id: string,
   name: string,
   token: string,
+  active: boolean,
   lastUsedDate: string
 }
 
@@ -11,12 +12,14 @@ export default class ApiKey {
   id: string
   name: string
   token: string
+  active: boolean
   lastUsedDate: string
 
   constructor(args: ConstructorArgs) {
     this.id = args.id
     this.name = args.name
     this.token = args.token
+    this.active = args.active
     this.lastUsedDate = args.lastUsedDate
   }
 
@@ -25,6 +28,7 @@ export default class ApiKey {
       id: this.id,
       name: this.name,
       token: this.token,
+      active: this.active,
       lastUsedDate: this.lastUsedDate
     }
   }
