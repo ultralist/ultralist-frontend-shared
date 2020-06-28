1298d9353c1e02847993a72a2f2c682a52e17dbd
Grant Ammons <gammons@gmail.com>
2020-06-28 06:22:51 -0400

    come up with rules for sorting todos

--- models/todoListGroup.js
+++ models/todoListGroup.js
@@ -19,11 +19,61 @@ export default class TodoListGroup {
   }
 
   sortedTodos(): Array<TodoItem> {
-    return this.todos.sort((t1, t2) => {
-      if (t1.isPriority) return -1
-      if (t2.isPriority) return 1
-      if (t1.dueDate() < t2.dueDate()) return 1
-      return -1
-    })
+    const sorted = []
+
+    // add prioritized, uncompleted, unarchived todos with a due date first
+    sorted.push(
+      this.todos
+        .filter(
+          t =>
+            t.isPriority && !t.archived && !t.completed && t.dueDate() !== null
+        )
+        .sort((t1, t2) => (t1.dueDate() < t2.dueDate() ? -1 : 1))
+    )
+
+    // add prioritized, uncompleted, unarchived todos due someday
+    sorted.push(
+      this.todos
+        .filter(
+          t =>
+            t.isPriority && !t.archived && !t.completed && t.dueDate() === null
+        )
+        .sort((t1, t2) => (t1.subject < t2.subject ? -1 : 1))
+    )
+
+    // then the uncompleted, unarchived todos that have a due date, sorted by due date
+    sorted.push(
+      this.todos
+        .filter(
+          t =>
+            !t.isPriority && !t.archived && !t.completed && t.dueDate() !== null
+        )
+        .sort((t1, t2) => (t1.dueDate() < t2.dueDate() ? -1 : 1))
+    )
+
+    // then the uncompleted, unarchived someday todos
+    sorted.push(
+      this.todos
+        .filter(
+          t =>
+            !t.isPriority && !t.archived && !t.completed && t.dueDate() === null
+        )
+        .sort((t1, t2) => (t1.subject < t2.subject ? -1 : 1))
+    )
+
+    // then the archived or completed todos without a due date
+    sorted.push(
+      this.todos
+        .filter(t => (t.archived || t.completed) && t.dueDate() !== null)
+        .sort((t1, t2) => (t1.dueDate() < t2.dueDate() ? -1 : 1))
+    )
+
+    sorted.push(
+      this.todos
+        .filter(t => (t.archived || t.completed) && t.dueDate() === null)
+        .sort((t1, t2) => (t1.subject < t2.subject ? -1 : 1))
+    )
+
+    return sorted.flat()
   }
 }
