1ff904424ec4023d90c7774198c94aa1a354dea5
Grant Ammons <gammons@gmail.com>
2020-10-21 06:19:44 -0400

    fold the eventsBackend into the eventCache

--- backend/eventCache.js
+++ backend/eventCache.js
@@ -1,12 +1,17 @@
 // @flow
 import TodoEvent from "../models/todoEvent"
+import { Backendable } from "./backends/backendable"
 
 export default class EventCache {
   cache: Array<TodoEvent>
+  backend: Backendable
+  token: string
 
-  constructor() {
+  constructor(backend: Backendable, token: string) {
     const cache = window.localStorage.getItem("eventCache")
     this.cache = cache ? cache : []
+    this.backend = backend
+    this.token = token
   }
 
   addItem(event: TodoEvent) {
@@ -19,6 +24,20 @@ export default class EventCache {
 
   toBackendJSON(): Array<Object> {
     return this.cache.map(e => e.toBackendJSON())
+  }
+
+  publishEvents() {
+    if (!navigator.onLine) return
+    if (this.cache.length === 0) return
+
+    return this.backend.apiRequest(
+      `api/v1/todo_lists/event_cache`,
+      "PUT",
+      this.token,
+      {
+        events: this.toBackendJSON()
+      }
+    )
   }
 
   clear() {
